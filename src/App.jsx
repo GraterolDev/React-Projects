eeabaad380728fad17ff85419a8c9797d95701e2
GraterolDev <graterolalejandro999@gmail.com>
2025-01-21 17:45:25 -0400

    Feat: Guardar datos en localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,15 @@ import { checkWinnerFrom } from "./logic/board.js"
 import { WinnerModal } from "./components/WinnerModal.jsx"
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-    )
-  const [turn, setTurn] = useState(TURN.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURN.X
+  })
 
   const [winner, setWinner] = useState(null)
 
@@ -17,6 +22,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURN.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const checkEndGame = (newBoard) => {
@@ -32,6 +40,9 @@ function App() {
 
     const newTurn = turn === TURN.X ? TURN.O : TURN.X
     setTurn(newTurn)
+
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
 
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
